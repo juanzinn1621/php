c064896196169724db026be83336f4070454acdd
juanzinn1621 <127741820+juanzinn1621@users.noreply.github.com>
2024-02-20 09:33:33 -0300

    Add files via upload

---
+++ primeiro_script.php
@@ -0,0 +1,10 @@
+<?php
+echo "SISTEMAS WEB I";
+$linguagem = "<br>PHP"; // Tipo String
+$tipo = "Linguagem Dinâmica!";
+echo $linguagem . " - " . $tipo;
+$versao = 17.0; // Variável tipo double 
+$ano = 2006; // Variável tipo inteiro
+echo "<br>Versão $versao - Ano: $ano";
+
+?>
